af376c959f14a0978808e64a7ca75cc46c9ebe48
Santiago-MZ <pgw10243@gmail.com>
2021-08-05 09:43:08 +0900

    1일 1문제 21.08.05

---
+++ b5585.py
@@ -0,0 +1,7 @@
+moneys = [500, 100, 50, 10, 5, 1]
+cost= 1000 - int(input())
+numbers = []
+for money in moneys:
+    numbers.append(cost//money)
+    cost -= (cost//money) * money
+print(sum(numbers))
