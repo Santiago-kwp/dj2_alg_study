19aff6a72bd7a8670e60fa24a51d35254d30ce2e
Santiago-MZ <pgw10243@gmail.com>
2021-08-13 00:39:31 +0900

    21.08.13(금) 코드 수정 및 설명 반영

--- b12782.py
+++ b12782.py
@@ -1,22 +1,16 @@
+'''
+1의 개수가 차이나면 매꿔야 되고 
+개수 차이를 제외한 자리수가 다른 경우를 2로 나누면 교환 성립 
+'''
 for tc in range(int(input())):
     N, M = input().split()
-    # 자리수가 다른데 쌍이면 교환 가능
-    # 1의 수가 다르면 추가 가능
-    cnt = 0
-    N_diff = 0
-    M_diff = 0
     tot_diff = 0
     for i in range(len(N)):
         if N[i] != M[i]:
             tot_diff += 1
-            # if N[i]:
-            #     N_diff += 1
-            # else:
-            #     M_diff += 1
     num_diff = abs(N.count('1') - M.count('1'))
     ans = (tot_diff - num_diff) //2 + num_diff
-    print(ans)
-        
+    print(ans)        
 
 
             
