07ab68b80065f170027e810940df644e2cc1b797
Santiago-MZ <pgw10243@gmail.com>
2021-08-21 14:06:10 +0900

    1일 1문제 21.08.22(일)

---
+++ b10773.py
@@ -0,0 +1,12 @@
+'''
+:= walrus operator 사용해보기
+'''
+import sys
+stack = []
+for _ in range(K:=int(sys.stdin.readline())):
+    if i:=int(sys.stdin.readline()):
+        stack.append(i)
+    else:
+        stack.pop()
+
+print(sum(stack) if stack else 0)
