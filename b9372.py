7696e43df968169e4bfa9f3b257cb61a21cfb133
Santiago-MZ <pgw10243@gmail.com>
2021-09-11 01:44:16 +0900

    1일 1문제 21.09.10(금)

---
+++ b9372.py
@@ -0,0 +1,15 @@
+'''
+상근이의 비행기 "종류"라는 걸 깨닫지 못하고
+재귀 구현같은걸로 삽질하다가 알게된 기본 최소 신장트리 개념
+'''
+import sys
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    for _ in range(M):
+        a = sys.stdin.readline()
+    print(N-1)
+
+
+
+        
